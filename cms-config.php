fd8f6b718f27ace6300134f891f3f9fcbc722bb4
Prashanth <prashanth@example.com>
2026-05-13 15:15:06 +0530

    Update CMS password in cms-config.php

--- cms-config.php
+++ cms-config.php
@@ -1,4 +1,4 @@
 <?php
 // Change this password before uploading the CMS to production.
-$CMS_PASSWORD_HASH = password_hash('ChangeThisPassword123!', PASSWORD_DEFAULT);
+$CMS_PASSWORD_HASH = password_hash('Timberlane@345!', PASSWORD_DEFAULT);
 
